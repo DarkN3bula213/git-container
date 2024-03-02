95f18291f868ace32d08f7bc92634ae8c63b7eb0
Ateeb Ahmed <a.ateeb87@gmail.com>
2024-03-02 11:01:11 +0000

    fix

--- src/lib/config/cors.ts
+++ src/lib/config/cors.ts
@@ -3,7 +3,15 @@ import { allowedOrigins, headers, methods } from '../constants/allowedOrigins';
 
 export const options: cors.CorsOptions = {
   origin: 'https://hps-admin.com', // Adjust to your front-end domain
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Explicitly define allowed methods
   credentials: true, // This is important for cookies
-  allowedHeaders: ['Content-Type', 'x-api-key', 'Authorization','x-access-token', 'x-refresh-token'], // Specify allowed headers
+  allowedHeaders: [
+    'Content-Type',
+    'x-api-key',
+    'Authorization',
+    'x-access-token',
+    'x-refresh-token',
+  ], // Specify allowed headers
   exposedHeaders: ['Set-Cookie'], // Expose headers if you need the client-side to read the Set-Cookie header
+  optionsSuccessStatus: 200,
 };
