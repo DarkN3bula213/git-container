82ff3fed3b1989ffe22aa31f3f186a0c4d0366f4
DarkN3bula213 <99716513+DarkN3bula213@users.noreply.github.com>
2025-01-04 16:46:08 +0500

    logger

--- src/lib/logger/logger.ts
+++ src/lib/logger/logger.ts
@@ -1,35 +1,111 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import colors from 'colors';
 import dayjs from 'dayjs';
+import fs from 'node:fs';
 import * as path from 'node:path';
-import winston from 'winston';
-import { defaultScope, logLevel } from './loggerConfig';
-import { customPrintf, customTimestampFormat } from './loggerFormats';
-import { consoleTransport, dailyRotateFileTransport } from './loggerTransports';
+import * as winston from 'winston';
+import DailyRotateFile from 'winston-daily-rotate-file';
+import { config } from '../config';
+
+// Define custom colors for log levels
+colors.setTheme({
+	info: 'black',
+	warn: 'yellow',
+	error: 'red',
+	debug: 'blue',
+	// Define custom styles for timestamp and message backgrounds
+	timestampBg: 'bgCyan', // This is an approximation; adjust as needed
+	infoMessage: 'white',
+	warnMessage: 'cyan',
+	errorMessage: 'magenta',
+	debugMessage: 'blue'
+});
+
+const levelColors: levelColorMap = {
+	info: 'yellow',
+	warn: 'cyan',
+	error: 'red',
+	debug: 'blue'
+};
+
+type levelColorMap = {
+	[key: string]: string;
+};
+
+// const timestamp = colors.grey(dayjs().format('| [+] | MM-DD HH:mm:ss'));
+
+// eslint-disable-next-line no-unused-vars
+const customTimestampFormat = winston.format((info, _opts) => {
+	info.timestamp = dayjs().format('| [+] | MM-DD HH:mm:ss');
+
+	return info;
+})();
+
+const customPrintf = winston.format.printf((info) => {
+	const timestamp = colors.grey(info.timestamp);
+	const levelColor = levelColors[info.level] || 'white';
+	const messageColor = (colors as any)[`${info.level}Message`];
+	const level = (colors as any)[levelColor](info.level.toUpperCase());
+	const message = messageColor ? messageColor(info.message) : info.message;
+
+	return `${timestamp} [${level}]: ${message}`;
+});
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+let dir = config.log.directory;
+if (!dir) dir = path.resolve('logs');
+if (!fs.existsSync(dir)) {
+	fs.mkdirSync(dir);
+}
+
+const logLevel = config.isProduction || config.isDocker ? 'error' : 'debug';
+const dailyRotateFile = new DailyRotateFile({
+	level: logLevel,
+	filename: `${dir}/%DATE%.log`,
+	datePattern: 'YYYY-MM-DD',
+	zippedArchive: true,
+	handleExceptions: true,
+	maxSize: '20m',
+	maxFiles: '14d',
+	format: config.isProduction
+		? winston.format.combine(
+				winston.format.timestamp(),
+				winston.format.errors({ stack: true }),
+				winston.format.json()
+			)
+		: winston.format.combine(
+				customTimestampFormat,
+				winston.format.errors({ stack: true }),
+				customPrintf
+			)
+});
 
 export class Logger {
-	public static readonly DEFAULT_SCOPE = defaultScope;
+	public static readonly DEFAULT_SCOPE = 'app';
 
 	private static readonly logger = winston.createLogger({
-		level: logLevel,
-		defaultMeta: {
-			scope: defaultScope
-		},
-		format: winston.format.combine(
-			customTimestampFormat,
-			winston.format.errors({ stack: true }),
-			customPrintf
-		),
-		transports: [consoleTransport, dailyRotateFileTransport],
-
-		exceptionHandlers: [
-			new winston.transports.File({ filename: 'logs/exceptions.log' })
-		],
-		rejectionHandlers: [
-			new winston.transports.File({ filename: 'logs/rejections.log' })
-		],
+		level: 'debug',
+		format: config.isProduction
+			? winston.format.combine(
+					winston.format.timestamp(),
+					winston.format.errors({ stack: true }),
+					winston.format.json() // Use JSON format in production
+				)
+			: winston.format.combine(
+					customTimestampFormat,
+					winston.format.errors({ stack: true }),
+					customPrintf // Use custom printf in development
+				),
+		transports: [new winston.transports.Console(), dailyRotateFile],
+		exceptionHandlers: [dailyRotateFile],
 		exitOnError: false
 	});
-
 	private static parsePathToScope(filepath: string): string {
 		let parsedPath = filepath;
 		if (parsedPath.indexOf(path.sep) >= 0) {
@@ -48,7 +124,6 @@ export class Logger {
 	constructor(scope?: string) {
 		this.scope = Logger.parsePathToScope(scope ?? Logger.DEFAULT_SCOPE);
 	}
-
 	public debug(message: string | object, ...args: any[]): void {
 		this.log('debug', message, args);
 	}
@@ -71,11 +146,16 @@ export class Logger {
 
 		if (typeof message === 'object') {
 			let formattedMessage = `${this.scope} \n`;
-			const lines = Object.entries(message).map(([key, value]) => {
-				const coloredKey = colors.cyan(key);
-				return `${timestamp} ${colors.cyan(':-----:')} ${coloredKey}: ${value}`;
-			});
-			formattedMessage += lines.join('\n');
+			if (!config.isProduction) {
+				const lines = Object.entries(message).map(([key, value]) => {
+					const coloredKey = colors.cyan(key);
+					return `${timestamp} ${colors.cyan(':-----:')} ${coloredKey}: ${value}`;
+				});
+				formattedMessage += lines.join('\n');
+			} else {
+				const messageString = JSON.stringify(message);
+				formattedMessage += `| + | ${messageString}`;
+			}
 			Logger.logger.log(level, formattedMessage);
 		} else {
 			const formattedMessage = `${message}`;
